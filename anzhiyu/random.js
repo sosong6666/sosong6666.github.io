bc1696d0657985bb9e095c8cfa7e8ef81606e58d
你的GitHub用户名 <GitHub>
2026-04-01 11:29:15 +0800

    Site updated: 2026-04-01 11:29:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/19/hello-world/"];function toRandomPost(){
+var posts=["2026/04/01/hello-world/","2026/04/01/recent-notes/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
